b56b30ad8671f60f16e76c2c63ea044b1eb54103
pacdong <8000pac@naver.com>
2020-12-09 09:36:41 +0900

    2020-12-09 / json server

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,31 +1,42 @@
-// 가짜 api 함수
-
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-const posts = [
-  {
-    id: 1,
-    title: "리덕스 미들웨어",
-    body: "리덕스 메들웨어 테스트하기",
-  },
-  {
-    id: 2,
-    title: "리덕스 thunk 사용하기",
-    body: "redux-thunk를 사용하여 비동기 작업 처리하기",
-  },
-  {
-    id: 3,
-    title: "리덕스 saga도 사용하기",
-    body: "redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배우기",
-  },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get("http://localhost:4000/posts");
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };
+
+// 가짜 api 함수
+// const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
+
+// const posts = [
+//   {
+//     id: 1,
+//     title: "리덕스 미들웨어",
+//     body: "리덕스 메들웨어 테스트하기",
+//   },
+//   {
+//     id: 2,
+//     title: "리덕스 thunk 사용하기",
+//     body: "redux-thunk를 사용하여 비동기 작업 처리하기",
+//   },
+//   {
+//     id: 3,
+//     title: "리덕스 saga도 사용하기",
+//     body: "redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배우기",
+//   },
+// ];
+
+// export const getPosts = async () => {
+//   await sleep(500);
+//   return posts;
+// };
+
+// export const getPostById = async (id) => {
+//   await sleep(500);
+//   return posts.find((post) => post.id === id);
+// };
